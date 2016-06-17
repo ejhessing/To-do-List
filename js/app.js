6f03fd53a28fdcc160ab83a656f373990ac284db
Erwin <ejhessing@gmail.com>
2016-06-18 00:06:36 +1200

    Added create and delete functionality

--- js/app.js
+++ js/app.js
@@ -1,14 +1,28 @@
-//Check off Specific todos By Clicking
-$("li").click(function(){
-  if($(this).css("color") === "rgb(128, 128, 128)"){
-      $(this).css({
-        color: "black",
-        textDecoration: "none"
-      });
-    } else {
-      $(this).css({
-        color: "gray",
-        textDecoration: "line-through"
-      });
-    }
+//create new to dos
+$("input[type='text'").keypress(function(evt){
+  //check if enter key is pressed
+  if(evt.which === 13){
+    //grab the new to do from input
+    var newToDo = $(this).val()
+    //clears the input text box
+    $(this).val("");
+    //create a new li and add the text
+    $("ul").append("<li><span>X</span> " + newToDo + "</li>");
+  }
 });
+
+
+//Check off Specific todos By Clicking
+$("ul").on("click", "li", function(){
+  $(this).toggleClass("completed");
+});
+
+
+
+// Click on X to Delete
+$("ul").on("click", "span", function(evt){
+    $(this).parent().fadeOut(500, function(){
+        $(this).remove();
+    });
+    evt.stopPropagation();
+})
